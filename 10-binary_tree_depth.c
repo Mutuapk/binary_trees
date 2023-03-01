e806b1dc683be813102c27459d23efcc2c5a52e0
Peter K. Mutua <mutuapeterk@gmail.com>
2023-03-01 12:13:39 +0300

    10.Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,20 +1,39 @@
-#include <stdlib.h>
 #include "binary_trees.h"
+size_t binary_tree_depth2(const binary_tree_t *tree);
+
 /**
-  * binary_tree_depth - measures the depth of a binary tree
-  * @tree: pointer to the root node to measure the depth
-  * Return: if tree is NULL return 0
-  */
+ * binary_tree_depth2 - measures the height of a binary tree
+ *
+ *@tree: pointer to tree
+ *
+ *Return: height
+ */
+
+size_t binary_tree_depth2(const binary_tree_t *tree)
+{
+  int cont1 = 0;
+
+  if (tree == NULL)
+    return (0);
+
+  if (tree->parent != NULL)
+    cont1 += binary_tree_depth2(tree->parent);
+
+  return (cont1 + 1);
+}
+
+/**
+ *binary_tree_depth - measures the depth of a binary tree
+ *
+ *@tree: pointer to tree
+ *
+ *Return: depth
+ */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t counter = 0;
+  if (tree == NULL)
+    return (0);
 
-	if (tree == NULL)
-		return (0);
-	while (tree->parent != NULL)
-	{
-		counter += 1;
-		tree = tree->parent;
-	}
-	return (counter);
+  return (binary_tree_depth2(tree) - 1);
 }
